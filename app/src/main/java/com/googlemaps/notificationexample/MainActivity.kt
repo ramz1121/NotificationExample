543296d6da588cc1e17b129400fbfc9fc10616b3
Ramya <ramya.rajendran1112@gmail.com>
2020-10-17 11:26:31 +1100

    Notification channel basic example

---
+++ app/src/main/java/com/googlemaps/notificationexample/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.googlemaps.notificationexample
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val channelID = "com.googlemaps.notificationexample.channel1"
+    private var notificationManager:NotificationManager?= null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        notificationManager =getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        createNotificationChannel(channelID, "DemoChannel","this is a demo")
+        button.setOnClickListener {
+            displayNotification()
+        }
+    }
+
+    private fun displayNotification() {
+        val notificationId = 33
+        val notification = NotificationCompat.Builder(this@MainActivity,channelID)
+                .setContentTitle("Demo Title")
+                .setContentText("This is a demo notification")
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .build()
+        notificationManager?.notify(notificationId,notification)
+
+    }
+
+    private fun createNotificationChannel(id: String, name: String, channelDescription:String){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            val importance : Int = NotificationManager.IMPORTANCE_HIGH
+            val channel =NotificationChannel(id,name,importance).apply {
+                description =channelDescription
+            }
+            notificationManager?.createNotificationChannel(channel)
+        }
+    }
+
+}
